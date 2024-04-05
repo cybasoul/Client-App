82b640874596104ea082d4f67c734f3e5db12a2b
Marlin Groves <marlindg365@gmail.com>
2024-04-05 15:46:16 +0200

    Database checkin with Clients.sql table

---
+++ Database1/dbo/Tables/Clients.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Clients] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]       NVARCHAR (MAX) NOT NULL,
+    [LastName]        NVARCHAR (MAX) NOT NULL,
+    [MobileNumber]    NVARCHAR (MAX) NOT NULL,
+    [IDNumber]        NVARCHAR (MAX) NOT NULL,
+    [PhysicalAddress] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database1/dbo/Tables/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [ProductName] NVARCHAR (MAX) NOT NULL,
+    [Price]       FLOAT (53)     NOT NULL,
+    [QTY]         INT            NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
